b888ef7d9965771b56642f8c15876a9fabb977f8
Kang Jiaming <jiaming_kang@outlook.com>
2023-12-15 10:10:37 +0800

    fix: escape

--- src/app/(main)/[locale]/legal/terms/page.tsx
+++ src/app/(main)/[locale]/legal/terms/page.tsx
@@ -3,17 +3,17 @@ export default function Page() {
     <article className="p-24">
       <h1 className="text-3xl font-bold">Terms of Usage</h1>
       <p>
-        Customer's terms and conditions (this "Agreement") are effective as of
+        Customer&apos;s terms and conditions (this &quot;Agreement) are effective as of
         the date you click the button or check the checkbox (or similar) to
-        confirm your acceptance of this Agreement (the "Effective Date").
+        confirm your acceptance of this Agreement (the &quot;Effective Date&quot;).
       </p>
       <p>
-        Subject to Customer's compliance with the terms and conditions of this
+        Subject to Customer&apos;s compliance with the terms and conditions of this
         Agreement, including, without limitation, the restrictions on use,
         access to and use of the Services will be provided to Customer on a
         non-exclusive, non-transferable, and non-sublicensable basis during the
         Subscription Term for authorized users only. Such use is limited to
-        Customer's internal business purposes and to the features and
+        Customer&apos;s internal business purposes and to the features and
         functionality specified in the Order.
       </p>
     </article>
