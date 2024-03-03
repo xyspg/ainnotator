9675423126eb0aa26c4157f342af823c090cd175
Kang Jiaming <jiaming_kang@outlook.com>
2024-03-03 12:56:55 +0800

    fix: use plain react hook

--- src/lib/hooks/use-user.tsx
+++ src/lib/hooks/use-user.tsx
@@ -1,30 +1,34 @@
+'use client'
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import useSWR from "swr";
+import {useEffect, useState} from "react";
+import {User} from "@supabase/supabase-js";
 
 const supabase = createClientComponentClient();
 
-const fetchUser = async () => {
-  const { data, error } = await supabase.auth.getUser();
-
-  if (error) {
-    throw new Error(error.message);
-  }
-
-  console.log(data.user)
-  return data.user;
-};
-
 export function useUser() {
-  const { data: user, error, isValidating } = useSWR('user', fetchUser, {
-    shouldRetryOnError: false,
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  });
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  return {
-    user,
-    loading: !user && isValidating,
-    error,
-  };
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        setLoading(true);
+        const { data, error } = await supabase.auth.getUser();
+        if (error) {
+          throw error;
+        }
+        setUser(data.user);
+      } catch (error: any) {
+        console.error('Error fetching user:', error.message);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getUser();
+  }, []); // Empty dependency array ensures this effect only runs once after the initial render
+
+  return { user, loading, error };
 }
